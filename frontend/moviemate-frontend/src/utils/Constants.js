a4edc2a88ef44165cb209148859e10a643ba0d1e
Metehan Sacakci <metehan.sacakci@ug.bilkent.edu.tr>
2022-03-06 13:34:02 +0300

    Constans.js and index.css
    Constans.js and index.css
    Metehan Saçakçı

---
+++ frontend/moviemate-frontend/src/utils/Constants.js
@@ -0,0 +1,14 @@
+class Constants  {
+    static BORDER_RADIUS = 10;
+    static ROKKIT_FONT_FAMILY = "Rokkitt, serif";
+    static WHITE = "#ffffff";
+    static BLACK = "#000000";
+    static MOVIEMATE_RED = "#9f4141";
+    static MOVIEMATE_GREEN = "#419f8c";
+    static MOVIEMATE_BLUE = "rgba(99, 134, 158, 0.9)";
+    static MOVIEMATE_BACKGROUND = "#161621";
+    static MOVIEMATE_NAVBAR_BACKGROUND = "#252525";    
+
+}
+
+export default Constants;
