d3c5f02a1d8ff7faa40a8e862fda9ae4e223ebe2
aslidinc <asli.dinc@ug.bilkent.edu.tr>
2022-04-30 17:14:10 +0300

    Admin controller and response model added

--- backend/moviemate-backend/src/main/java/com/moviematebackend/moviematebackend/controllers/AdminController.java
+++ backend/moviemate-backend/src/main/java/com/moviematebackend/moviematebackend/controllers/AdminController.java
@@ -1,8 +1,12 @@
 package com.moviematebackend.moviematebackend.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.moviematebackend.moviematebackend.exception.UserServiceException;
+import com.moviematebackend.moviematebackend.models.responseMoldes.SelectAdmin;
+import com.moviematebackend.moviematebackend.utils.DatabaseConnection;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @CrossOrigin
 @RestController
@@ -29,5 +33,55 @@ public class AdminController {
 
         admin oluşturma silme yapılacak burda
      */
+
+    @GetMapping // http://localhost:8080/admin?email=adminasli@gmail.com
+    public SelectAdmin selectAdmin ( @RequestParam( value = "email" ) String email ) {
+
+        try {
+            String query = "SELECT * FROM Admin WHERE email = '" + email + "'";
+            Statement statement = DatabaseConnection.getInstance().getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery( query );
+            SelectAdmin selectAdmin;
+            if ( resultSet.next() ) {
+                selectAdmin = new SelectAdmin( resultSet.getInt( "admin_id" ) ,
+                        resultSet.getString( "first_name" ) ,
+                        resultSet.getString( "last_name" ) ,
+                        resultSet.getString( "email" ) ,
+                        resultSet.getString( "password" ) );
+                return selectAdmin;
+            }else{
+                throw new UserServiceException( "Data is not found!" );
+            }
+        } catch ( Exception e ) {
+            throw new UserServiceException( e.getMessage() );
+        }
+    }
+
+    @PostMapping // http://localhost:8080/admin?email=adminasli@gmail.com&password=123456&name=Aslı&surname=Dinç
+    public Boolean insertAdmin ( @RequestParam( value = "email" ) String email ,
+                                    @RequestParam( value = "password" ) String password ,
+                                    @RequestParam( value = "name" ) String name ,
+                                    @RequestParam( value = "surname" ) String surname ) {
+        try {
+            Statement statement = DatabaseConnection.getInstance().getConnection().createStatement();
+
+            String statementString = "INSERT INTO Admin( `email`, `password`, `first_name`, `last_name` ) " +
+                    "VALUES " +
+                    "(" +
+                    "'" + email +
+                    "', '" + password +
+                    "', '" + name +
+                    "', '" + surname +
+                    "'" +
+                    ");";
+
+            statement.executeUpdate( statementString );
+
+            return true;
+        } catch ( Exception e ) {
+            return false;
+        }
+    }
+
 
 }
