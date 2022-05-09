789fc8d1e04ae71ea58a4a09c11eb327f1cb1a91
aslidinc <asli.dinc@ug.bilkent.edu.tr>
2022-05-09 23:43:30 +0300

    Rating tab in profile added

--- frontend/moviemate-frontend/src/screens/profileScreens/Ratings.js
+++ frontend/moviemate-frontend/src/screens/profileScreens/Ratings.js
@@ -1,9 +1,23 @@
 import { Grid } from "@mui/material";
 import React from 'react'
+import RatingMovieCard from "../../components/RatingMovieCard";
 import Constants from "../../utils/Constants";
 
 export default function Ratings(props) {
     return <Grid container style={{ display: "flex", justifyContent: "center", color: Constants.WHITE }}>
-
+        <Grid container >
+            <Grid item xs={6} >
+                <div>
+                    <RatingMovieCard />
+                    <RatingMovieCard />
+                </div>
+            </Grid>
+            <Grid item xs={6}>
+                <div>
+                    <RatingMovieCard />
+                    <RatingMovieCard />
+                </div>
+            </Grid>
+        </Grid>
     </Grid>
 }
